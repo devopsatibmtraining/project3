9c344824ed62ad0c628e5cb210cf4cad4772aa39
devopsatibmtraining <48584667+devopsatibmtraining@users.noreply.github.com>
2019-03-25 12:39:22 +0800

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,20 +1,32 @@
 package com.mycompany.app;
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Before;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+
+public class AppTest {
+
+   private HelloWorld h;
+	
+   @Before
+   public void setUp() throws Exception 
+   {
+      h = new HelloWorld();
+   }
+
+   @Test
+   public void testHelloEmpty() 
+   {
+      assertEquals(h.getName(),"");
+      assertEquals(h.getMessage(),"Hello!");
+   }
+	
+   @Test
+   public void testHelloWorld() 
+   {
+      h.setName("World");
+      assertEquals(h.getName(),"World");
+      assertEquals(h.getMessage(),"Hello World!");
+   }
 }
